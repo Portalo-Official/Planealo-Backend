95be677407cc70442a6ce63c6c6718d8ec73ad93
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-08 00:16:37 +0200

    probando @SpringBootAplication

--- Usuarios/src/test/java/com/planealo/harninasantiago/repository/UserRepositoryTest.java
+++ Usuarios/src/test/java/com/planealo/harninasantiago/repository/UserRepositoryTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
 @ActiveProfiles("test")
 class UserRepositoryTest {
 
