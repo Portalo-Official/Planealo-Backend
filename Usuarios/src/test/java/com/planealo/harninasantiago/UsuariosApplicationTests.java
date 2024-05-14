660a914fc63846d262b1a31e03d4ae9a1890fe4d
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-14 11:03:26 +0200

    reseteo perfiles maven

--- Usuarios/src/test/java/com/planealo/harninasantiago/UsuariosApplicationTests.java
+++ Usuarios/src/test/java/com/planealo/harninasantiago/UsuariosApplicationTests.java
@@ -2,8 +2,10 @@ package com.planealo.harninasantiago;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UsuariosApplicationTests {
 
 	@Test
